05a68d550f0fb2fc61b1db53d01e5f1d2639a8a4
richjdsmith <rich@richjdsmith.ca>
2017-03-06 10:51:00 -0800

    File loader implemented. Basic CSV Parsing implemented

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,8 @@
 puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
-
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+lines.each_with_index do |line, index|
+  next if index == 0
   columns = line.split(",")
   name = columns[2]
   puts name

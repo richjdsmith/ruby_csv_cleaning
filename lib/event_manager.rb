94d31661af7783e6d00c7205177daeb762d4e41c
richjdsmith <rich@richjdsmith.ca>
2017-03-06 12:12:49 -0800

    Display legislators by zipcode. Legislator moved to separeate method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,34 @@
 require "csv"
-puts "EventManager Initialized!"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  legislator_names = legislators.collect do |legislator|
+    "#{legislator.first_name} #{legislator.last_name}"
+  end
+
+  legislator_string = legislator_names.join(", ")
+end
+
+
+
+
+puts "EventManager Initialized!"
 
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 contents.each do |row|
   name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} | #{zipcode} \n #{legislators}"
 end

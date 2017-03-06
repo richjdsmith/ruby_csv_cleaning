8386fc41edee4ff810ee2b74d250906a301daf9b
richjdsmith <rich@richjdsmith.ca>
2017-03-06 10:54:57 -0800

    Changed to use CSV Gem as parser

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,9 @@
 require "csv"
 puts "EventManager Initialized!"
 
-contents = CSV.open("event_attendees.csv", headers: true)
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 contents.each do |row|
-  name = row[2]
-  puts name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
